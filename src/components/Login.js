51d10ca8c8c8ab641234336ed8f55cf7042c58a8
bocky <shtyi037@yahoo.com.tw>
2022-05-08 21:02:17 +0800

    [render] s2

---
+++ src/components/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Login extends React.Component{  //繼承
+    render(){ //返回內容
+        return <p>Login Component</p>
+    }
+}
+
+
+export default Login; // 暴漏出去給其他地方使用
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Login from './components/Login'
 
-class Login extends React.Component{  //繼承
-    render(){ //返回內容
-        return <p>Login Component</p>
-    }
-}
 
 ReactDOM.render(<Login/>, document.getElementById("root")) // 引用上方定義的Login組建
