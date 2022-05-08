250b3003b2a9fa9d65c91b99be45dfb507978e33
bocky <shtyi037@yahoo.com.tw>
2022-05-08 20:55:53 +0800

    [render] s1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert('Hello, React!!'); 
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Login extends React.Component{  //繼承
+    render(){ //返回內容
+        return <p>Login Component</p>
+    }
+}
+
+ReactDOM.render(<Login/>, document.getElementById("root")) // 引用上方定義的Login組建
